338b620efe307ff2d32450ce05fdd916c95118fd
jorgeluis <jorgel.dominguez03@gmail.com>
2023-03-28 21:55:31 -0500

    pagina login

---
+++ Grupo1_LAB1_GTICS/src/main/java/com/example/grupo1_lab1_gtics/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.example.grupo1_lab1_gtics.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login (){
+        return "login";
+    }
+}
