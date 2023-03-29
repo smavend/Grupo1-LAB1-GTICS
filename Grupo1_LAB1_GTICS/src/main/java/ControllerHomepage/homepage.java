327caaee7d459e87af48e11698070e5ed9ed2d2f
r.b4rr10s <a20202073@pucp.edu.pe>
2023-03-28 21:25:47 -0500

    Controller

---
+++ Grupo1_LAB1_GTICS/src/main/java/ControllerHomepage/homepage.java
@@ -0,0 +1,12 @@
+package ControllerHomepage;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class homepage {
+    @GetMapping("/homepage")
+    public String homePage(){
+        return "homepage";
+    }
+}
