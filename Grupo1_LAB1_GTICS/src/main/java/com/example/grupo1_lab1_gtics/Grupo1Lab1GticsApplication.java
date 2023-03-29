51ee347c5d23e90aca40e98743b0de3226fbd145
unknown <amanrique068@gmail.com>
2023-03-28 20:56:33 -0500

    agregar proyecto

---
+++ Grupo1_LAB1_GTICS/src/main/java/com/example/grupo1_lab1_gtics/Grupo1Lab1GticsApplication.java
@@ -0,0 +1,13 @@
+package com.example.grupo1_lab1_gtics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo1Lab1GticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo1Lab1GticsApplication.class, args);
+	}
+
+}
---
+++ Grupo1_LAB1_GTICS/src/test/java/com/example/grupo1_lab1_gtics/Grupo1Lab1GticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grupo1_lab1_gtics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo1Lab1GticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
