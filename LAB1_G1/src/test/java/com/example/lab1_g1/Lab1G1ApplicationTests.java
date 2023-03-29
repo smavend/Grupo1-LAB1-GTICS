aa4b2925d2736b404054d8e3d7cc99834cd7f728
unknown <amanrique068@gmail.com>
2023-03-28 20:48:04 -0500

    Proyecto vacio

---
+++ LAB1_G1/src/test/java/com/example/lab1_g1/Lab1G1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab1_g1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1G1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
